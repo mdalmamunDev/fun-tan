0bacf728c996abb195fcf0dc385268ab711dbb81
Mamun <mdalmamun5354@gmail.com>
2025-03-09 10:34:19 +0600

    "

---
+++ database/migrations/2025_03_09_041616_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('industry_id');
+            $table->foreignId('thumbnail_id')->nullable();
+            $table->foreignId('files_id');
+            $table->string('genre');
+            $table->string('tag')->nullable();
+            $table->integer('views')->default(0);
+            $table->decimal('rating', 3, 1)->default(0.0);
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
